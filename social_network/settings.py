7e10395ccbbb71fa0ae83c0e0303aaf6d4d039c2
taha230 <taha.hamedani8@gmail.com>
2025-12-12 18:36:09 +0330

    add statics

--- social_network/settings.py
+++ social_network/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'   # or os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = f'{BASE_URL}media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
